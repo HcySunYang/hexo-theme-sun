860caea7085eab904a8fa79bfa1d5eb613ef702b
HcySunYang <HcySunYang@outlook.com>
2017-08-09 14:48:33 +0800

    Article page style adjustment

--- themes/hcysun/source/js/script.js
+++ themes/hcysun/source/js/script.js
@@ -1,16 +1,24 @@
 // 点击搜索按钮
 $('.search-box').on('click', function () {
-    $('.search-input').toggleClass('active');
-    $('.search-mark').toggleClass('active');
-    $('.search-input-el input[type=search]').attr('placeholder', 'Search...').focus();
-    return false;
+  $('.search-input').toggleClass('active');
+  $('.search-mark').toggleClass('active');
+  $('.search-input-el input[type=search]').attr('placeholder', 'Search...').focus();
+  return false;
 })
 
 $('.search-input').on('click', function () {
-    return false;
+  return false;
 })
 
 $(document).on('click', function () {
-    $('.search-input').removeClass('active');
-    $('.search-mark').removeClass('active');
+  $('.search-input').removeClass('active');
+  $('.search-mark').removeClass('active');
 })
+
+// 初始化 highlight.js
+$(document).ready(function() {
+  $('pre code').each(function(i, block) {
+    console.log(block)
+    hljs.highlightBlock(block)
+  });
+});
